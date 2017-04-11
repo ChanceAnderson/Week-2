14488d6bc640102a2ea4cdfa6cb5f1986a7e25c9
Chance Anderson <chance.cea.anderson@gmail.com>
2017-04-10 21:24:24 -0700

    added location objects

--- app.js
+++ app.js
@@ -2,25 +2,41 @@
 
 console.log('Greenlight');
 
-var groceryList = {
-  title: 'Dinner',
-  items: ['Eggs', 'Flour','Fruit'],
+var seaTacEstimate = Math.floor(Math.random() * 24) + 3;
+var seaCenterEstimate = Math.floor(Math.random() * 38) + 11;
+var capitolHillEstimate = Math.floor(Math.random() * 38) + 20;
+var alkiEstimate = Math.floor(Math.random() * 16) + 2;
+
+var pike = {
+  minHourlyCust: 23,
+  maxHourlyCust: 65,
+  avgCookiesCustom: 6.3,
+};
+var seaTac = {
+  minHourlyCust: 3,
+  maxHourlyCust: 24,
+  avgCookiesCustom: 1.2,
+};
+var seaCenter = {
+  minHourlyCust: 11,
+  maxHourlyCust: 38,
+  avgCookiesCustom: 3.7,
+};
+var capitolHill = {
+  minHourlyCust: 20,
+  maxHourlyCust: 38,
+  avgCookiesCustom: 2.3,
+};
+var alki = {
+  minHourlyCust: 2,
+  maxHourlyCust: 16,
+  avgCookiesCustom: 4.6,
 };
 
-var heading = document.getElementById('title');
-heading.textContext = groceryList.title;
-heading.setAttribute('class', 'cool box button');
-
-var itemsUl = document.getElementById('items');
-//create li tag for each string in groceryList.items
-//fill that li with the string from the groceryList.items[i]
-//put the new li at the end of ingredientsUl using appendChild
-
-
-var liEl;
-for (var i = 0; i < groceryList.items.length; i++){
-  console.log('groceryItems.list of i', groceryList.item[i]);
-  liEl = document.createElement('li');
-  liEl.textContext = groceryList.items[i];
-  ingredientsUl.appendChild(liEl); 
+for(var i = 1; i <= 6; i++){
+  if(i >= 12){
+    i = 1;
+  }
+  var pikeEstimate = Math.floor(Math.random() * 65) + 23;
+  console.log(i + ' am: ' + pikeEstimate + ' cookies');
 };
