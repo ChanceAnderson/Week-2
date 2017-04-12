070f6327b833bb6166e32fc0891516f1c7d7ad7f
Chance Anderson <chance.cea.anderson@gmail.com>
2017-04-12 10:31:34 -0700

    worked on js functions

--- app.js
+++ app.js
@@ -2,43 +2,62 @@
 
 console.log('Greenlight');
 
-var seaTacEstimate = Math.floor(Math.random() * 24) + 3;
-var seaCenterEstimate = Math.floor(Math.random() * 38) + 11;
-var capitolHillEstimate = Math.floor(Math.random() * 38) + 20;
-var alkiEstimate = Math.floor(Math.random() * 16) + 2;
-
 var pike = {
+  openAt: 6,
+  closeAt: 20,
   minHourlyCust: 23,
   maxHourlyCust: 65,
   avgCookiesCustom: 6.3,
   data: [],
+
 };
 
-for(var i; i <= 12; i++){
-    var pikeEstimate = Math.floor(Math.random() * 65) + 23;
-    console.log(i + ' am: ' + pikeEstimate + ' cookies');
-    pike.data.push(pikeEstimate);
-  };
-
-
-
 var seaTac = {
+  openAt: 6,
+  closeAt: 20,
   minHourlyCust: 3,
   maxHourlyCust: 24,
   avgCookiesCustom: 1.2,
+
 };
 var seaCenter = {
+  openAt: 6,
+  closeAt: 20,
   minHourlyCust: 11,
   maxHourlyCust: 38,
   avgCookiesCustom: 3.7,
 };
 var capitolHill = {
+  openAt: 6,
+  closeAt: 20,
   minHourlyCust: 20,
   maxHourlyCust: 38,
   avgCookiesCustom: 2.3,
 };
 var alki = {
+  openAt: 6,
+  closeAt: 20,
   minHourlyCust: 2,
   maxHourlyCust: 16,
   avgCookiesCustom: 4.6,
 };
+
+
+//Cookies for every hour thru closing
+var cookieHourly = function(place){
+
+  //creates random number of customers
+  var randomNumCust = function(place){
+    return Math.floor(Math.random() * (place.maxHourlyCust - place.minHourlyCust + 1)) + place.minHourlyCust;
+  };
+
+  //create simulated number of cookies per hour
+  var simCookieHr = place.avgCookiesCustom * randomNumCust(place);
+
+  for(var i = 6; i <= 20; i++){
+    console.log(i + ' am: ' + simCookieHr + ' cookies');
+
+  };
+};
+
+console.log(cookieHourly(alki));
